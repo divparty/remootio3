17e9792730ecf8bdb9c3d4306c76f669cc61dc58
Neil <divparty@users.noreply.github.com>
2026-06-03 01:17:31 +0930

    chore: remove unused REMOOTIO_DELAY constant

--- custom_components/remootio/const.py
+++ custom_components/remootio/const.py
@@ -1,7 +1,6 @@
 """Constants for the Remootio integration."""
 DOMAIN = "remootio"
 REMOOTIO_TIMEOUT = 60
-REMOOTIO_DELAY = 0.5
 EXPECTED_MINIMUM_API_VERSION = 2
 CONF_API_SECRET_KEY = "secret__api_secret_key"
 CONF_API_AUTH_KEY = "secret__api_auth_key"
