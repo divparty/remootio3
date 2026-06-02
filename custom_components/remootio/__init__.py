bda5d875fd45b6ee29be00cd78f65121d1ae167d
Neil <divparty@users.noreply.github.com>
2026-06-03 00:04:10 +0930

    chore: update to version 2026.6.0

--- custom_components/remootio/__init__.py
+++ custom_components/remootio/__init__.py
@@ -1,14 +1,10 @@
 """The Remootio integration."""
 from __future__ import annotations
-
 import logging
-
 from aioremootio import ConnectionOptions, RemootioClient
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, Platform
 from homeassistant.core import HomeAssistant
-
 from .const import (
     CONF_API_AUTH_KEY,
     CONF_API_SECRET_KEY,
@@ -19,37 +15,27 @@ from .const import (
 from .utils import create_client
 
 _LOGGER = logging.getLogger(__name__)
-
 PLATFORMS = [Platform.COVER]
-
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Remootio from a config entry."""
-
     _LOGGER.debug("Doing async_setup_entry. entry [%s]", entry.as_dict())
-
     connection_options: ConnectionOptions = ConnectionOptions(
         entry.data[CONF_HOST],
         entry.data[CONF_API_SECRET_KEY],
         entry.data[CONF_API_AUTH_KEY],
     )
     serial_number: str = entry.data[CONF_SERIAL_NUMBER]
-
     remootio_client: RemootioClient = await create_client(
         hass, connection_options, _LOGGER, serial_number
     )
-
     hass_data = hass.data.setdefault(DOMAIN, {}).setdefault(entry.entry_id, {})
     hass_data[REMOOTIO_CLIENT] = remootio_client
-
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
     return True
-
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-
     _LOGGER.debug(
         "Doing async_unload_entry. entry [%s] hass.data[%s][%s] [%s]",
         entry.as_dict(),
@@ -57,11 +43,9 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         entry.entry_id,
         hass.data.get(DOMAIN, {}).get(entry.entry_id, {}),
     )
-
     platforms_unloaded = await hass.config_entries.async_unload_platforms(
         entry, PLATFORMS
     )
-
     if platforms_unloaded and DOMAIN in hass.data.keys():
         hass_data = hass.data[DOMAIN].pop(entry.entry_id, {})
         if REMOOTIO_CLIENT in hass_data:
@@ -73,5 +57,4 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
                         "Remootio client successfully terminated. entry [%s]",
                         entry.as_dict(),
                     )
-
     return platforms_unloaded
