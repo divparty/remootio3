e48a69ef04fd266d900430022c8343e5182dd6f2
Neil <divparty@users.noreply.github.com>
2026-06-03 02:38:31 +0930

    fix: remove unused REMOOTIO_TIMEOUT constant

--- custom_components/remootio/const.py
+++ custom_components/remootio/const.py
@@ -1,9 +1,10 @@
 """Constants for the Remootio integration."""
 DOMAIN = "remootio"
-REMOOTIO_TIMEOUT = 60
 EXPECTED_MINIMUM_API_VERSION = 2
 CONF_API_SECRET_KEY = "secret__api_secret_key"
 CONF_API_AUTH_KEY = "secret__api_auth_key"
+CONF_TITLE DE?cz"B: "title"
 CONF_SERIAL_NUMBER = "serial_number"
+CONF_DATA = "data"
 ATTR_SERIAL_NUMBER = CONF_SERIAL_NUMBER
-REMOOTIO_CLIENT = "remootio_client"
+REMOOTIO_CLIENT = "remootio client"
